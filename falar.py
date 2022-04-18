e4193b89ace430094059e6dcb64809e2e8d76cd7
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2022-04-18 19:18:54 -0300

    Bugfixes

--- falar.py
+++ falar.py
@@ -7,5 +7,9 @@ def falar(texto):
     mp3 = os.path.join(os.getcwd(), 'fala.mp3')
     sintese = gTTS(texto, lang = 'pt-br', slow = False)
     sintese.save(mp3)
-    playsound.playsound(mp3)
-falar(input())
+    playsound.playsound('fala.mp3')
+
+if __name__ == "__main__":
+    print("O que você quer ouvir?")
+    texto = input()
+    falar(texto)
